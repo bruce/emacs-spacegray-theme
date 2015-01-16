6f66ef207254699433afb54e65ec0b12f4ab0070
Brandon Carrell <brandoncarrell@gmail.com>
2015-01-16 08:45:29 -0500

    Add helm theming

--- spacegray-theme.el
+++ spacegray-theme.el
@@ -588,7 +588,19 @@
    `(term-color-blue    ((,class (:foreground ,blue :background ,blue))))
    `(term-color-magenta ((,class (:foreground ,purple :background ,purple))))
    `(term-color-cyan    ((,class (:foreground ,aqua :background ,aqua))))
-   `(term-color-white   ((,class (:foreground ,background :background ,background)))))
+   `(term-color-white   ((,class (:foreground ,background :background ,background))))
+
+   ;; helm
+   `(helm-buffer-saved-out ((,class (:inherit warning))))
+   `(helm-buffer-size ((,class (:foreground ,yellow))))
+   `(helm-buffer-not-saved ((,class (:foreground ,orange))))
+   `(helm-buffer-process ((,class (:foreground ,aqua))))
+   `(helm-buffer-directory ((,class (:foreground ,blue))))
+   `(helm-ff-directory ((,class (:foreground ,aqua))))
+   `(helm-candidate-number ((,class (:foreground ,red))))
+   `(helm-selection ((,class (:inherit highlight))))
+   `(helm-separator ((,class (:foreground ,purple))))
+   `(helm-source-header ((,class (:weight bold :foreground ,orange :height 1.25)))))
 
   (custom-theme-set-variables
    'spacegray
